744b00dbd097b20bdc46814b0fcddb7dc7ff5be9
Billi Ramirez <ramirezbilly42@outlook.com>
2020-01-21 11:05:11 -0600

    breaking out action creator

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,47 @@
 import React from "react";
+import { connect } from "react-redux";
+import { Todo, fetchTodos } from "../actions";
+import { StoreState } from "../reducers";
 
-export default class App extends React.Component {
+interface AppProps {
+  todos: Todo[];
+  fetchTodos(): any;
+}
+
+class _App extends React.Component<AppProps> {
+  onButtonClick = (): void => {
+    this.props.fetchTodos();
+  };
+
+  renderList(): JSX.Element[] {
+    return this.props.todos.map(todo => (
+      <div style={{ marginTop: "5px", cursor: "pointer" }} key={todo.id}>
+        {todo.title}
+      </div>
+    ));
+  }
+
   render() {
-    return <div>Hi there!</div>;
+    return (
+      <div
+        style={{
+          display: "flex",
+          margin: "0 auto",
+          flexDirection: "column",
+          alignItems: "center"
+        }}
+      >
+        <button onClick={this.onButtonClick}>Fetch</button>
+        {this.renderList()}
+      </div>
+    );
   }
 }
+
+const mapStateToProps = ({ todos }: StoreState): { todos: Todo[] } => {
+  return {
+    todos
+  };
+};
+
+export const App = connect(mapStateToProps, { fetchTodos })(_App);
