1e5f7f68ddcc76524b70fef1fa564368b0bfe6eb
Billi Ramirez <ramirezbilly42@outlook.com>
2020-01-21 11:24:15 -0600

    tracking loading

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,21 +1,47 @@
 import React from "react";
 import { connect } from "react-redux";
-import { Todo, fetchTodos } from "../actions";
+import { Todo, fetchTodos, deleteTodo } from "../actions";
 import { StoreState } from "../reducers";
 
 interface AppProps {
   todos: Todo[];
-  fetchTodos(): any;
+  fetchTodos: Function; // this is cheat
+  deleteTodo: typeof deleteTodo;
 }
 
-class _App extends React.Component<AppProps> {
+interface AppState {
+  fetching: boolean;
+}
+
+class _App extends React.Component<AppProps, AppState> {
+  constructor(props: AppProps) {
+    super(props);
+
+    this.state = { fetching: false };
+  }
+
+  componentDidUpdate(prevProps: AppProps): void {
+    if (!prevProps.todos.length && this.props.todos.length) {
+      this.setState({ fetching: false });
+    }
+  }
+
   onButtonClick = (): void => {
     this.props.fetchTodos();
+    this.setState({ fetching: true });
+  };
+
+  onTodoClick = (id: number): void => {
+    this.props.deleteTodo(id);
   };
 
   renderList(): JSX.Element[] {
-    return this.props.todos.map(todo => (
-      <div style={{ marginTop: "5px", cursor: "pointer" }} key={todo.id}>
+    return this.props.todos.map((todo: Todo) => (
+      <div
+        style={{ marginTop: "5px", cursor: "pointer" }}
+        key={todo.id}
+        onClick={() => this.onTodoClick(todo.id)}
+      >
         {todo.title}
       </div>
     ));
@@ -32,6 +58,7 @@ class _App extends React.Component<AppProps> {
         }}
       >
         <button onClick={this.onButtonClick}>Fetch</button>
+        {this.state.fetching ? "Loading..." : null}
         {this.renderList()}
       </div>
     );
@@ -44,4 +71,4 @@ const mapStateToProps = ({ todos }: StoreState): { todos: Todo[] } => {
   };
 };
 
-export const App = connect(mapStateToProps, { fetchTodos })(_App);
+export const App = connect(mapStateToProps, { fetchTodos, deleteTodo })(_App);
